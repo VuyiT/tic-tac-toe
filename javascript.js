4e26b1a58c4df103b659af2676fe9e82b2465057
Avuyile Tiki <avuyiletiki14@gmail.com>
2025-06-18 12:03:40 +0200

    Create a function to print the board on the console

--- javascript.js
+++ javascript.js
@@ -35,5 +35,13 @@ function Gameboard() {
         }
     };
 
-    return {getBoard, markCell, resetBoard};
+    const printBoard = () => {
+        console.log(`${board[0][0] || "1"} | ${board[0][1] || "2"} | ${board[0][2] || "3"} 
+            ---+---+---
+            ${board[1][0] || "4"} |${board[1][1] || "5"} |${board[1][2] || "6"} 
+            ---+---+---
+            ${board[2][0] || "7"} |${board[2][1] || "8"} |${board[2][2] || "9"}`);
+    };
+
+    return {getBoard, markCell, resetBoard, printBoard};
 }
